b476cfe961e8f4df3e201527e0c568c5d71cd7ca
digitab <90602348+gusgunawan728@users.noreply.github.com>
2026-06-09 09:52:53 +0700

    Add quiz questions with images and answers

---
+++ script.js
@@ -0,0 +1,58 @@
+const soal = [
+
+{
+gambar:"img/berjalan.png",
+pertanyaan:"Contoh gerak berjalan adalah ....",
+pilihan:[
+"Berjalan ke kelas",
+"Mengayunkan tangan",
+"Memutar badan"
+],
+jawaban:0
+},
+
+{
+gambar:"img/berlari.png",
+pertanyaan:"Saat lomba lari, Beni bergerak dengan cara ....",
+pilihan:[
+"Melompat",
+"Berlari",
+"Membungkuk"
+],
+jawaban:1
+},
+
+{
+gambar:"img/melompat.png",
+pertanyaan:"Contoh gerak melompat adalah ....",
+pilihan:[
+"Melompati tali",
+"Menangkap bola",
+"Menendang bola"
+],
+jawaban:0
+},
+
+{
+gambar:"img/meloncat.png",
+pertanyaan:"Gerak meloncat dilakukan dengan ....",
+pilihan:[
+"Dua kaki",
+"Satu kaki sebagai tumpuan",
+"Duduk"
+],
+jawaban:1
+},
+
+{
+gambar:"img/membungkuk.png",
+pertanyaan:"Membungkukkan badan termasuk gerak ....",
+pilihan:[
+"Lokomotor",
+"Manipulatif",
+"Nonlokomotor"
+],
+jawaban:2
+}
+
+];
